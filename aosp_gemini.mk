20c402cbf48966cacf6c2f196db9bc888db28213
rk2810 <rohan.kumquat@gmail.com>
2017-08-27 07:18:36 +0000

    adapt to aosp changes for O

--- aosp_gemini.mk
+++ aosp_gemini.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,18 +14,18 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Provide meaningful APN configuration
+PRODUCT_COPY_FILES := device/xiaomi/gemini/configs/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from gemini device
-$(call inherit-product, device/xiaomi/gemini/device.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gemini
+PRODUCT_NAME := aosp_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
